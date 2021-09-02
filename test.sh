4ac9a4ada5a912256cba8e52fc6990789c82c5c2
Juan Gonzalez <jrg2156@gmail.com>
2021-09-01 18:52:38 -0700

    Refactor django testing

--- test.sh
+++ test.sh
@@ -8,14 +8,8 @@ print_in_cyan() {
     printf "\x1B[36m%b\x1B[0m\n" "$*"
 }
 
-print_in_cyan "Testing Python 3.6"
-PYTHON_VERSION=3.6 docker-compose run --rm  test ./manage.py test --snapshot-update
-
-print_in_cyan "\nTesting Python 3.7"
-PYTHON_VERSION=3.7 docker-compose run --rm  test ./manage.py test --snapshot-update
-
-print_in_cyan "\nTesting Python 3.8"
-PYTHON_VERSION=3.8 docker-compose run --rm  test ./manage.py test --snapshot-update
-
-print_in_cyan "\nTesting Python 3.9"
-PYTHON_VERSION=3.9 docker-compose run --rm  test ./manage.py test --snapshot-update
+for PYTHON_VERSION in 3.6 3.7 3.8 3.9
+do
+    print_in_cyan "\nTesting Python $PYTHON_VERSION"
+    PYTHON_VERSION=$PYTHON_VERSION docker-compose run --rm  test ./test.sh
+done
