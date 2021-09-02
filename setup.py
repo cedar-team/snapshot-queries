b45bcf17fe87bced1702b79d942a38d9015fc135
Juan Gonzalez <jrg2156@gmail.com>
2021-09-02 08:32:45 -0700

    Update setup.py metadata

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name="snapshot-queries",
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
+    author="Cedar",
+    author_email="support@cedar.com",
     license="MIT",
-    description="A small example package",
+    description="A tool for capturing queries executed in Django and SqlAlchemy",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/cedar-team/snapshot-queries",
     project_urls={
-        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+        "Bug Tracker": "https://github.com/cedar-team/snapshot-queries",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -25,6 +25,11 @@ setuptools.setup(
     python_requires=">=3.6",
     install_requires=["sqlparse>=0.4.1", "attrs>=20.3.0", "pygments>=2.8.1"],
     extras_require={
-        "test": ["snapshottest==0.5.1", "psycopg2-binary==2.8.6", "nose", "tox"]
+        "test": [
+            "snapshottest==0.5.1",
+            "psycopg2-binary==2.8.6",
+            "nose==1.3.7",
+            "tox==3.24.3",
+        ]
     },
 )
