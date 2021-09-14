201baa40183d6a00374acd253eaa3cf4c7511714
Juan Gonzalez <jrg2156@gmail.com>
2021-09-13 20:22:38 -0700

    Add command-line arguments to test.sh

--- test.sh
+++ test.sh
@@ -8,9 +8,39 @@ print_in_cyan() {
     printf "\x1B[36m%b\x1B[0m\n" "$*"
 }
 
-for PYTHON_VERSION in 3.6  3.7  3.8  3.9
+PYTHON_VERSIONS="3.6  3.7  3.8  3.9"
+TEST_SQLALCHEMY=true
+TEST_DJANGO=true
+
+for i in "$@"; do
+  case $i in
+    --python-version=*)
+      PYTHON_VERSIONS="${i#*=}"
+      shift # past argument=value
+      ;;
+    --django)
+      TEST_SQLALCHEMY=false
+      shift
+      ;;
+    --sqlalchemy)
+      TEST_DJANGO=false
+      shift
+      ;;
+    *)
+      # unknown option
+      ;;
+  esac
+done
+
+for PYTHON_VERSION in $PYTHON_VERSIONS
 do
     print_in_cyan "\nTesting Python $PYTHON_VERSION"
-    PYTHON_VERSION=$PYTHON_VERSION docker-compose up --build test-django
-    PYTHON_VERSION=$PYTHON_VERSION docker-compose up --build test-sqlalchemy
+
+    if [ "$TEST_DJANGO" = true ]; then
+        PYTHON_VERSION=$PYTHON_VERSION docker-compose up --build test-django
+    fi
+
+    if [ "$TEST_SQLALCHEMY" = true ]; then
+        PYTHON_VERSION=$PYTHON_VERSION docker-compose up --build test-sqlalchemy
+    fi
 done
