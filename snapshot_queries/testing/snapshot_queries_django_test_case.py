92805eee5a60e68020cfdaea80dc3b0c7984c95b
Juan Gonzalez <jrg2156@gmail.com>
2022-04-17 22:28:38 +0000

    feat: add AssertQueriesMatchMixin class

---
+++ snapshot_queries/testing/snapshot_queries_django_test_case.py
@@ -0,0 +1,15 @@
+import snapshottest.django
+
+from .assert_queries_match_mixin import AssertQueriesMatchMixin
+
+
+class SnapshotQueriesDjangoTestCase(
+    snapshottest.django.TestCase, AssertQueriesMatchMixin
+):
+    @property
+    def module(self):
+        return self._snapshot.module
+
+    @property
+    def test_name(self):
+        return self._snapshot.test_name
--- tests/test_django/settings/settings.py
+++ tests/test_django/settings/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from collections import OrderedDict
 
