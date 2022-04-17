68d6969db42d204ede3fe03f7421b156c8ae6a58
Juan Gonzalez <jrg2156@gmail.com>
2022-04-17 23:47:05 +0000

    style: remove bad formatting

--- tests/test_sqlalchemy/test_sqlite.py
+++ tests/test_sqlalchemy/test_sqlite.py
@@ -2,7 +2,8 @@ from datetime import date
 from pathlib import Path
 
 from snapshottest import TestCase
-from sqlalchemy import Column, Date, Integer, MetaData, String, Table, create_engine
+from sqlalchemy import (Column, Date, Integer, MetaData, String, Table,
+                        create_engine)
 
 from snapshot_queries import snapshot_queries
 
