a3cc3fbe5d121b11a180487e5da6c4ce6020f4c7
Juan Gonzalez <jrg2156@gmail.com>
2022-04-27 16:38:41 -0400

    fix: pytest wont error if snapshottest not installed

--- snapshot_queries/testing/__init__.py
+++ snapshot_queries/testing/__init__.py
@@ -1,11 +1,12 @@
 # flake8: noqa
 import snapshot_queries.optional_dependencies
 
-from .snapshot_queries_test_case import SnapshotQueriesTestCase
+if snapshot_queries.optional_dependencies.SNAPSHOTTEST_INSTALLED:
+    from .snapshot_queries_test_case import SnapshotQueriesTestCase
 
-__all__ = ["SnapshotQueriesTestCase"]
+    __all__ = ["SnapshotQueriesTestCase"]
 
-if snapshot_queries.optional_dependencies.DJANGO_INSTALLED:
-    from .snapshot_queries_django_test_case import SnapshotQueriesDjangoTestCase  # noqa
+    if snapshot_queries.optional_dependencies.DJANGO_INSTALLED:
+        from .snapshot_queries_django_test_case import SnapshotQueriesDjangoTestCase  # noqa
 
-    __all__.append("SnapshotQueriesDjangoTestCase")
+        __all__.append("SnapshotQueriesDjangoTestCase")
